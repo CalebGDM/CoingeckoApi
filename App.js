0f73f6bd8c30e55c3b6259b23d357536f2626bc5
Caleb <daikuweb@gmail.com>
2021-06-18 15:53:33 -0500

    Search Bar Funtional And Drag To Update

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 //import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, FlatList, StatusBar, TextInput } from 'react-native';
 import CoinItem from './components/coinItem'
-import Header from './components/header'
-import search from './components/header'
 
 export default function App() {
 
   const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+  const [refreshing, setRefreshing] = useState(false)
 
   const loadData = async () => {
     const res = await fetch(
@@ -24,20 +24,37 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#141414" />
-      <Header />
+      <View style={styles.headerContainer}>
+            <Text style={styles.title}>KriptoTracker</Text>
+            <TextInput 
+            style={styles.searchInput}
+            placeholder='Search a Coin'
+            placeholderTextColor='#858585'
+            onChangeText={(text) => setSearch(text)}
+            />
+      </View>
+      
       <View style={styles.list}>
         <FlatList
-
           data={
-            coins
+            coins.filter((coin) => 
+            coin.name.toLowerCase().includes(search.toLowerCase()) || 
+            coin.symbol.toLowerCase().includes(search.toLocaleLowerCase())
+            )           
           }
           renderItem={({ item }) => {
             return <CoinItem coin={item} />
           }}
+          refreshing={refreshing}
           showsVerticalScrollIndicator={false}
+          onRefresh={async() => {
+            setRefreshing(true)
+            await loadData();
+            setRefreshing(false)
+          }}
         />
       </View>
-
+      
 
     </View>
   );
@@ -48,10 +65,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#141414',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
   list: {
+
     width: '90%',
     paddingTop: 50
-  }
+  },
+  headerContainer:{
+        position: 'absolute',
+        top: 10,
+        //zIndex: 100,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '90%',
+        marginBottom: 10
+       
+    },
+    title:{
+        color: '#ffffff',
+        fontSize: 20
+    },
+    searchInput:{
+        color: '#ffffff',
+        borderBottomColor: '#4657CE',
+        borderBottomWidth: 2,
+        width: '40%',
+        textAlign: 'center'
+    } 
 });
